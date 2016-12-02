7a8aeba8f79daa30563e06c80f41860db436f327
MicahelLuthor <micahelluthor@163.com>
2016-12-02 15:40:54 +0800

    抓取1000条后暂停10分钟。

--- spider/QQ/UserInfoSpider.php
+++ spider/QQ/UserInfoSpider.php
@@ -53,7 +53,7 @@ class UserInfoSpider extends AbstractSpirder {
      * @see AbstractSpirder::generateTasks()
      */
     protected function generateTasks() {
-        for ($i=0; $i<500; $i++ ) {
+        for ($i=0; $i<1000; $i++ ) {
             if ( $this->maxUid < $this->uid  ) {
                 break;
             }
